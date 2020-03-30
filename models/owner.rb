6c18a232529a68cdbe1e5ae661b1e479c8ee08dd
github username <github email address>
2020-03-30 13:27:22 -0400

    Done.

--- models/owner.rb
+++ models/owner.rb
@@ -1,3 +1,6 @@
 class Owner < ActiveRecord::Base
   has_many :cats
 end
+
+#Whenever we use a has_many we also have to use the belongs_to 
+#The foreign key always sits on the table of the object that belongs to. In this case, because cats belong to an owner, the owner_id becomes a column in the cats table.
